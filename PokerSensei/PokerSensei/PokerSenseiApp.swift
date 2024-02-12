222c88cf9159356943e38678583e567870c3b3e5
jade-davis <jadealexie12@gmail.com>
2024-02-12 08:59:42 -0600

    basic IOS app

---
+++ PokerSensei/PokerSensei/PokerSenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerSenseiApp.swift
+//  PokerSensei
+//
+//  Created by Jade Davis on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerSenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokerSensei/PokerSenseiUITests/PokerSenseiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PokerSenseiUITestsLaunchTests.swift
+//  PokerSenseiUITests
+//
+//  Created by Jade Davis on 2/12/24.
+//
+
+import XCTest
+
+final class PokerSenseiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
