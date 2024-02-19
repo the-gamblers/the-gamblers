e1e65d8526ea62dd4fcda27bd613bf2ef967561e
jade-davis <jadealexie12@gmail.com>
2024-02-19 10:34:00 -0600

    fixed app

--- PokerSensei/Server/PokerSenseiAPI/obj/Debug/net8.0/PokerSenseiAPI.AssemblyInfo.cs
+++ PokerSensei/Server/PokerSenseiAPI/obj/Debug/net8.0/PokerSenseiAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokerSenseiAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f291983269558147eac5a62b32fb32c5a1d178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e316a138c4b252bd590661bcf323c7aa7845c20a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokerSenseiAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokerSenseiAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
