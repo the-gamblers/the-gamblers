371bdb1c9fdf2973593d8163759438f2447c7bab
jade-davis <jadealexie12@gmail.com>
2024-02-22 11:24:35 -0600

    basic replay list page

---
+++ PokerSensei/Client/PokerSen/ReplayListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReplayListView.swift
+//  PokerSen
+//
+//  Created by Jade Davis on 2/21/24.
+//
+
+import SwiftUI
+
+struct ReplayListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReplayListView()
+}
---
+++ PokerSensei/Client/PokerSen/ReplayTableView.swift
@@ -0,0 +1,18 @@
+//
+//  ReplayTableView.swift
+//  PokerSen
+//
+//  Created by Jade Davis on 2/21/24.
+//
+
+import SwiftUI
+
+struct ReplayTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReplayTableView()
+}
