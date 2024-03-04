fc09c7437fc10c4595773c4bb7ff79f29415c257
jade-davis <jadealexie12@gmail.com>
2024-03-03 21:13:29 -0600

    part 2 of prev commit

--- PokerSensei/Client/ChessMaster/ProgressPage.swift
+++ PokerSensei/Client/ChessMaster/ProgressPage.swift
@@ -18,14 +18,18 @@ struct ProgressPage: View {
 
     var body: some View {
         NavigationView {
+            
             ZStack {
                 // Background image
-                Image("background-pic")
-                    .resizable()
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-                    .opacity(0.5)
-
+                //Image("background-pic")
+                 //   .resizable()
+                   // .scaledToFill()
+                    //.edgesIgnoringSafeArea(.all)
+                    //.opacity(0.5)
+                Color(red: 238/255, green: 238/255, blue: 238/255)
+                    .opacity(0.2)
+                    .ignoresSafeArea()
+                    .overlay(
                 // rounded rectangle background
                 ScrollView {
                     VStack(alignment: .leading, spacing: 20) {
@@ -43,6 +47,8 @@ struct ProgressPage: View {
                                     Text("username00")
                                         .font(.title2)
                                     Spacer()
+                                    Image(systemName: "flame.fill")
+                                    Text("6")
                                 }
                                 .padding(.horizontal)
                                 .padding(.top, 20)
@@ -55,10 +61,120 @@ struct ProgressPage: View {
                                 .pickerStyle(SegmentedPickerStyle())
                                 .padding()
 
-                                VStack(alignment: .leading) {
-                                    Text("Wins: 4")
-                                    Text("Losses: 7")
-                                    Text(" ")
+                                VStack(alignment: .leading, spacing: 20) {
+
+                                    HStack {
+                                        Spacer()
+                                        // losses
+                                        VStack{
+                                            Image(systemName: "hand.thumbsdown.fill")
+                                                .resizable()
+                                                .frame(width: 20, height: 20)
+                                            Text("38")
+                                                .font(.headline)
+                                            Text("Losses")
+                                                .font(.subheadline)
+                                        }
+                                        .padding(20)
+                                        .frame(width: 90, height: 100)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray.opacity(0.5), lineWidth: 1))
+                                        Spacer()
+                                        // wins
+                                        VStack{
+                                            Image(systemName: "trophy.fill")
+                                                .resizable()
+                                                .frame(width: 20, height: 20)
+                                            Text("67")
+                                                .font(.headline)
+                                            Text("Wins")
+                                                .font(.subheadline)
+                                        }
+                                        .padding(20)
+                                        .frame(width: 90, height: 100)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray.opacity(0.5), lineWidth: 1))
+                                        Spacer()
+                                        // ties
+                                        VStack{
+                                            Image(systemName: "person.line.dotted.person.fill")
+                                            Text("4")
+                                                .font(.headline)
+                                            Text("Ties")
+                                                .font(.subheadline)
+                                        }
+                                        .padding(20)
+                                        .frame(width: 90, height: 100)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray.opacity(0.5), lineWidth: 1))
+                                        Spacer()
+                                    }
+                                    LazyVGrid(columns: [GridItem(.flexible(), spacing: 20), GridItem(.flexible(), spacing: 20)], spacing: 20) {
+                                            // Avg Time
+                                            VStack {
+                                                Image(systemName: "clock.fill")
+                                                Text("Avg Time")
+                                                    .font(.headline)
+                                                Text("2 hours")
+                                                    .font(.subheadline)
+                                            }
+                                            .padding(20)
+                                            .frame(width:155, height: 90)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                                            )
+                                            
+                                            // Best Game
+                                            VStack {
+                                                Image(systemName: "star.fill")
+                                                Text("Best Game")
+                                                    .font(.headline)
+                                                Text("Match XYZ")
+                                                    .font(.subheadline)
+                                            }
+                                            .padding(20)
+                                            .frame(width:155, height: 90)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                                            )
+                                            
+                                            // Total Games
+                                            VStack {
+                                                Image(systemName: "number")
+                                                Text("Total Games")
+                                                    .font(.headline)
+                                                Text("109")
+                                                    .font(.subheadline)
+                                            }
+                                            .padding(20)
+                                            .frame(width:155, height: 90)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                                            )
+                                            
+                                            // Favorite Move
+                                        VStack(alignment: .center) {
+                                                Image(systemName: "heart.fill")
+                                                Text("Favorite Move")
+                                                    .font(.headline)
+                                                Text("Queen's Gambit")
+                                                    .font(.subheadline)
+                                            }
+                                            .padding(20)
+                                            .frame(width:155, height: 90)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                                            )
+                                        Spacer()
+                                    }
+                                    .padding(.top, 20)
                                 }
                                 .padding(.leading)
                             }
@@ -66,11 +182,15 @@ struct ProgressPage: View {
                         .padding(.horizontal)
                         .padding(.top, 30)
 //                        .padding(.bottom, 20)
+                        
                     }
-                    .navigationTitle("Progress Page")
+                    
+                    .navigationTitle("PROGRESS")
                 }
                 
-            }
+           ) }
+            
+            
         }
     }
 }
