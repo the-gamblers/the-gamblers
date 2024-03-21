48dc154c0747f43ed21563b5456525a52afdfc04
jade-davis <jadealexie12@gmail.com>
2024-03-21 11:59:17 -0500

    added a playable view

---
+++ PokerSensei/Client/ChessMaster/PlayableView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayableView.swift
+//  ChessMaster
+//
+//  Created by Jade Davis on 3/21/24.
+//
+
+import SwiftUI
+
+struct PlayableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayableView()
+}
