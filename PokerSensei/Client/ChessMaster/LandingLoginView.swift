82270444af41d34896b5202743e2683108db6538
jade-davis <jadealexie12@gmail.com>
2024-03-21 13:02:51 -0500

    create login page

---
+++ PokerSensei/Client/ChessMaster/LandingLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingLoginView.swift
+//  ChessMaster
+//
+//  Created by Jade Davis on 3/21/24.
+//
+
+import SwiftUI
+
+struct LandingLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingLoginView()
+}
