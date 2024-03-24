3eb1023e9be7796854a891277c90f231f99950cc
jade-davis <jadealexie12@gmail.com>
2024-03-24 16:20:11 -0500

    Profile view added on pages

---
+++ PokerSensei/Client/ChessMaster/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ChessMaster
+//
+//  Created by Jade Davis on 3/22/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
